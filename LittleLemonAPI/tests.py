1091c84420ce6b441afedf167dfc97effa804349
allenkays <allenkays@tutanota.com>
2025-06-25 12:00:13 +0300

    Add test cases

--- LittleLemonAPI/tests.py
+++ LittleLemonAPI/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Category, MenuItem
+
 
 # Create your tests here.
+class MenuItemTests(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(slug="test", title="Test Category")
+        self.menu_item = MenuItem.objects.create(
+            title="Test Item", price=10.99, featured=True, category=self.category
+        )
+
+    def test_menu_item_creation(self):
+        self.assertEqual(self.menu_item.title, "Test Item")
+        self.assertEqual(self.menu_item.price, 10.99)
